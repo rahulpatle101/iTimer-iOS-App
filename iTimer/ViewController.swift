f7b4312e279384c85b0022f1259ce12ad69cfc19
Rahul Patle <rahulpatle101@gmail.com>
2016-01-12 15:56:45 -0700

    Initial Commit- got the basic interface setup

--- iTimer/ViewController.swift
+++ iTimer/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var lapResetBtn: UIButton!
+    @IBOutlet var startStopBtn: UIButton!
+    @IBOutlet var lapsTable: UITableView!
+    @IBOutlet var stopWatchLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startStop(sender: AnyObject) {
+    }
+   
+    @IBAction func lapReset(sender: AnyObject) {
+    }
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Lap"
+        cell.detailTextLabel?.text = "00:00:00"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 }
 
